a9eff6b2909878597502e9453af4c6519cd98875
Thomas Dahlberg <thomas.dahlberg8@gmail.com>
2020-08-14 21:52:11 -0400

    adds gallery menu and images

--- src/components/painting.js
+++ src/components/painting.js
@@ -1,89 +1,28 @@
-import React from "react"
+import React, { Component } from "react"
 import workStyles from "./work.module.scss"
+let workContent = require('./workContent') 
 
-const Painting = () => {
-    return (
-        <div className={workStyles.container}>
-            <div className={workStyles.content}>
-                <img src="./images/painting/slopsink.jpg" alt="soma sculpture"/>
-                <p><em>Sôma</em>; 2020; acrylic, dirt, pencil and foam board, 6in x 9in x 9in</p>      
+class Painting extends Component {
+    constructor(props) {
+        super(props) 
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div className={workStyles.container}>
+                <div className={workStyles.content}>
+                    <img src="./images/painting/slopsink.jpg" alt="soma sculpture"/>
+                    <p><em>Sôma</em>; 2020; acrylic, dirt, pencil and foam board, 6in x 9in x 9in</p>      
+                </div>
+                <div className={workStyles.gallery}>
+                        {workContent.workLib.map(({ type, title, file })=>
+                            <img src={`./images/${type}/${file}.jpg`} alt={title}/>
+                        )}
+                </div>
             </div>
-            <div className={workStyles.gallery}>
-                <ul>
-                    <li>One</li>
-                    <li>Two</li>
-                    <li>Three</li>
-                    <li>Four</li>
-                    <li>Five</li>
-                    <li>Six</li>
-                    <li>Seven</li>
-                    <li>One</li>
-                    <li>Two</li>
-                    <li>Three</li>
-                    <li>Four</li>
-                    <li>Five</li>
-                    <li>Six</li>
-                    <li>Seven</li>
-                    <li>One</li>
-                    <li>Two</li>
-                    <li>Three</li>
-                    <li>Four</li>
-                    <li>Five</li>
-                    <li>Six</li>
-                    <li>Seven</li>
-                    <li>One</li>
-                    <li>Two</li>
-                    <li>Three</li>
-                    <li>Four</li>
-                    <li>Five</li>
-                    <li>Six</li>
-                    <li>Seven</li>
-                    <li>One</li>
-                    <li>Two</li>
-                    <li>Three</li>
-                    <li>Four</li>
-                    <li>Five</li>
-                    <li>Six</li>
-                    <li>Seven</li>
-                    <li>One</li>
-                    <li>Two</li>
-                    <li>Three</li>
-                    <li>Four</li>
-                    <li>Five</li>
-                    <li>Six</li>
-                    <li>Seven</li>
-                    <li>One</li>
-                    <li>Two</li>
-                    <li>Three</li>
-                    <li>Four</li>
-                    <li>Five</li>
-                    <li>Six</li>
-                    <li>Seven</li>
-                    <li>One</li>
-                    <li>Two</li>
-                    <li>Three</li>
-                    <li>Four</li>
-                    <li>Five</li>
-                    <li>Six</li>
-                    <li>Seven</li>
-                    <li>One</li>
-                    <li>Two</li>
-                    <li>Three</li>
-                    <li>Four</li>
-                    <li>Five</li>
-                    <li>Six</li>
-                    <li>Seven</li>
-                    <li>One</li>
-                    <li>Two</li>
-                    <li>Three</li>
-                    <li>Four</li>
-                    <li>Five</li>
-                    <li>Six</li>
-                    <li>Seven</li>
-                </ul>
-            </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default Painting
---
+++ src/components/workContent.js
@@ -0,0 +1,76 @@
+let workLib = [
+                {
+                    title: "Slop Sink",
+                    file: "slopsink",
+                    year: 2020,
+                    materials: "acrylic on canvas",
+                    dimensions: "16in x 20in",
+                    type: "painting"
+                },
+                {
+                    title: "The Painting",
+                    file: "Rm68_1",
+                    year: 2019,
+                    materials: "acrylic on paper",
+                    dimensions: "11in x 15in",
+                    type: "painting"
+                },
+                {
+                    title: "Vestibule",
+                    file: "Rm68_2",
+                    year: 2019,
+                    materials: "acrylic on paper",
+                    dimensions: "11in x 15in",
+                    type: "painting"
+                },
+                {
+                    title: "Boxed Lunch",
+                    file: "Rm68_3",
+                    year: 2019,
+                    materials: "acrylic on paper",
+                    dimensions: "11in x 15in",
+                    type: "painting"
+                },
+                {
+                    title: "Prison",
+                    file: "Rm68_4",
+                    year: 2019,
+                    materials: "acrylic on paper",
+                    dimensions: "11in x 15in",
+                    type: "painting"
+                },
+                {
+                    title: "Alert",
+                    file: "Rm68_5",
+                    year: 2019,
+                    materials: "acrylic on paper",
+                    dimensions: "11in x 15in",
+                    type: "painting"
+                },
+                {
+                    title: "Treehouse",
+                    file: "Rm68_6",
+                    year: 2019,
+                    materials: "acrylic on paper",
+                    dimensions: "11in x 15in",
+                    type: "painting"
+                },
+                {
+                    title: "Plop",
+                    file: "Rm68_7",
+                    year: 2019,
+                    materials: "acrylic on paper",
+                    dimensions: "11in x 15in",
+                    type: "painting"
+                },
+                {
+                    title: "Lean-To",
+                    file: "Rm68_8",
+                    year: 2019,
+                    materials: "acrylic on paper",
+                    dimensions: "11in x 15in",
+                    type: "painting"
+                }
+
+            ]  
+exports.workLib = workLib
