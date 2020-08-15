5453554e0abd5a0ac4157466233291b132959dab
Thomas Dahlberg <thomas.dahlberg8@gmail.com>
2020-08-14 23:25:13 -0400

    gallery image onclick enlargment functions

--- src/pages/painting.js
+++ src/pages/painting.js
@@ -7,7 +7,21 @@ let workContent = require('../components/workContent')
 class Painting extends Component {
     constructor(props) {
         super(props) 
-        this.state = {};
+        this.state = {
+            paintings: workContent.workLib
+        };
+    }
+    imageRef = React.createRef();
+    captionRef = React.createRef();
+
+    handleImageSelect = (e) => {
+        console.log(e.target.id);
+        const selectedWork = this.state.paintings[e.target.id];
+        const imageNode = this.imageRef.current;
+        const captionNode = this.captionRef.current;
+        imageNode.src = `./images/${selectedWork.type}/${selectedWork.file}.jpg`;
+        imageNode.alt = `${selectedWork.title}`;
+        captionNode.innerHTML = `<em>${selectedWork.title}</em>; ${selectedWork.year}; ${selectedWork.materials}; ${selectedWork.dimensions}`;
     }
 
     render() {
@@ -16,12 +30,12 @@ class Painting extends Component {
                 <Head title="Home" />
                 <div className={workStyles.container}>
                     <div className={workStyles.content}>
-                        <img src="./images/painting/slopsink.jpg" alt="soma sculpture"/>
-                        <p><em>Sôma</em>; 2020; acrylic, dirt, pencil and foam board, 6in x 9in x 9in</p>      
+                        <img ref={this.imageRef} src="./images/painting/slopsink.jpg" alt={workContent.workLib[0].title}/>
+                        <p ref={this.captionRef}><em>Sôma</em>; 2020; acrylic, dirt, pencil and foam board, 6in x 9in x 9in</p>      
                     </div>
                     <div className={workStyles.gallery}>
-                            {workContent.workLib.map(({ type, title, file })=>
-                                <img src={`./images/${type}/${file}.jpg`} alt={title}/>
+                            {workContent.workLib.map(({ type, title, file }, idx)=>
+                                <img key={idx} id={idx} onClick={this.handleImageSelect} src={`./images/${type}/${file}.jpg`} alt={title}/>
                             )}
                     </div>
                 </div>
