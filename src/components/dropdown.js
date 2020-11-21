4ec7b1b171573c8370eea3b28c1a78f9e6d20a9a
Thomas Dahlberg <thomas.dahlberg8@gmail.com>
2020-11-21 08:38:30 -0500

    updates dependencies, fixes deprecations, adds icon link to helmet

--- src/components/dropdown.js
+++ src/components/dropdown.js
@@ -1,4 +1,4 @@
-import React, { Fragment, Component } from 'react';
+import React, { Component } from 'react';
 import { Link } from 'gatsby';
 import styles from './Dropdown.module.scss';
 
--- src/components/head.js
+++ src/components/head.js
@@ -18,6 +18,7 @@ const Head = ({ title }) => {
         <div>
             <Helmet title={`${title} | ${data.site.siteMetadata.title}`}>
                 <link rel="icon" href={favicon}/>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
             </Helmet>
         </div>
     )
