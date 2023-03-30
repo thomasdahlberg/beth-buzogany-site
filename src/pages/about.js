76f1e89c674ed1c6329791021b5f7349d2430228
Thomas Dahlberg <thomas.dahlberg8@gmail.com>
2023-03-30 18:38:03 -0400

    Updates headshot and bio in about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,20 +5,19 @@ import styles from "../styles/about.module.scss";
 const About = () => {        
     return (
         <Layout title="About">
-            <section className={styles.section}>
-                <h3>About Me</h3>
-                <img className={styles.image} src="./beth-headshot.jpg" alt="headshot of Beth Buzogany"/>
-                <p>Beth Buzogany has over twenty years of experience as a food stylist and home economist. She has been engaged as a food stylist for projects of all types, including national advertising campaigns, television commercials, packaging, cookbooks, and editorial features.</p>
-                <p>Beth has coordinated sampling for live television audiences, developed recipes, and appeared on-air to demonstrate cooking techniques and recipe preparation. Her impressive client list includes such well-known enterprises as Pillsbury, The Biltmore Estate, The Pampered Chef and The Oprah Show.</p>
-                <p>Beth combines her expert culinary and artistic talents with her innovative problem-solving skills to create images that are not only mouth-watering but also exceed the clients' expectations every time. Her sense of humor and comfortable personality make for a pleasant day on set.</p>
-                <p>Beth holds a Bachelor of Science degree in Home Economics from Erskine College in South Carolina, and she also studied Hospitality Management. Before focusing on food styling, she was a flight attendant, consumer educator, and commercial producer.</p>
-                <p>Beth resides on a mini-farm in the mountains of North Carolina with her husband, cat, dog, flock of chickens and guineas, and two hives of bees. She works locally in Charlotte, Atlanta, and Chicago and travels for engagements all over the country. She and her husband, Buz, have two grown children. Her interests include painting, gardening, tennis, and hiking.</p>
-            </section>
-            <section className={styles.section}>
-                <h3>Contact</h3>
-                <a href="tel:+18473021506">847-302-1506</a>
-                <a href="mailto:bethbuzogany@gmail.com">bethbuzogany@gmail.com</a>
-            </section>
+            <div className={styles.container}>
+                <section className={styles.section}>
+                    <h3>About Me</h3>
+                    <img className={styles.image} src="./beth-headshot.jpg" alt="headshot of Beth Buzogany"/>
+                    <p>Beth Buzogany's degree in Home Economics and her background as a consumer educator and  commercial producer laid a solid foundation for her future as a food stylist; her artistic eye, steady hand, and creativity proved to be good tools on set. She began her career in Los Angeles, assisting one of the country's leading food stylists and then transitioned to styling in Chicago, working on packaging and advertising campaigns, both still and motion, for national clients. A final move returned Beth and her husband Buz to the mountains of western NC where they met, and she now works throughout the southeast, the mid-Atlantic, and Chicago. She is known for her attention to detail, her sense of humor, and for always having a Plan B, and usually a Plan C.</p>
+                    <p>On her days off, if you stop by Full Circle Farm, you may find her working in her raised bed gardens, tending her bees, painting with pastels, or preparing to go hiking in the mountains. She and Buz enjoy traveling, especially to visit their grandchildren.</p>
+                </section>
+                <section className={styles.section}>
+                    <h3>Contact</h3>
+                    <a href="tel:+18473021506">847-302-1506</a>
+                    <a href="mailto:bethbuzogany@gmail.com">bethbuzogany@gmail.com</a>
+                </section>
+            </div>
         </Layout>
     )
 }
